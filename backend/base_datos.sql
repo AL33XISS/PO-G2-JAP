7e551f521903521cea31204191dbdcbcdabd0654
sebastiansyf <sebastiansyft@gmail.com>
2025-11-26 16:16:05 -0300

    Correcciones finales

--- backend/base_datos.sql
+++ backend/base_datos.sql
@@ -70,11 +70,12 @@ CREATE TABLE IF NOT EXISTS comentarios (
     FOREIGN KEY (producto_id) REFERENCES productos(id) ON DELETE CASCADE
 );
 
--- Datos de ejemplo en usuarios
+-- Usuarios
 INSERT INTO usuarios (email, password, nombre) VALUES
 ('sebastiansilvayferrer@JAP', 'jap314', 'Sebastián Silva'),
 ('agustinfourcade@JAP', 'jap314', 'Agustín Fourcade'),
-('alexissarsamendi@JAP', 'jap314', 'Alexis Sarsamendi');
+('alexissalsamendi@JAP', 'jap314', 'Alexis Salsamendi'),
+('invitado@JAP', 'jap314', 'Usuario Invitado');
 
 -- Insertar datos de ejemplo en categorías
 INSERT INTO categorias (id, nombre, descripcion, imagen, cantidad_productos) VALUES
@@ -82,3 +83,23 @@ INSERT INTO categorias (id, nombre, descripcion, imagen, cantidad_productos) VAL
 (102, 'Juguetes', 'Encuentra aquí los mejores precios para niños/as', 'img/toys_index.jpg', 7),
 (103, 'Muebles', 'Muebles antiguos, nuevos y para armar', 'img/furniture_index.jpg', 10),
 (104, 'Herramientas', 'Herramientas para todo tipo de trabajo', 'img/tools_index.jpg', 5);
+
+
+-- Productos completos
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM productos;
+
+INSERT INTO productos (id, nombre, descripcion, precio, moneda, imagen_principal, vendidos, categoria_id) VALUES
+(50741, 'Chevrolet Onix Joy', 'Generación 2019', 13500, 'USD', 'img/prod50741_1.jpg', 102, 101),
+(50742, 'Fiat Way', 'Auto de múltiples versiones', 14500, 'USD', 'img/prod50742_1.jpg', 85, 101),
+(50743, 'Suzuki Celerio', 'Economía de combustible', 12200, 'USD', 'img/prod50743_1.jpg', 43, 101),
+(50744, 'Peugeot 208', 'Diseño y tecnología', 15500, 'USD', 'img/prod50744_1.jpg', 65, 101),
+(60801, 'Pelota de fútbol', 'Profesional', 25, 'USD', 'img/prod60801_1.jpg', 234, 102),
+(60802, 'Set de LEGO', '500 piezas', 45, 'USD', 'img/prod60802_1.jpg', 156, 102),
+(60803, 'Muñeca Barbie', 'Con accesorios', 30, 'USD', 'img/prod60803_1.jpg', 189, 102),
+(60804, 'Auto control remoto', 'Alta velocidad', 55, 'USD', 'img/prod60804_1.jpg', 98, 102),
+(50921, 'Silla de oficina', 'Ergonómica', 120, 'USD', 'img/prod50921_1.jpg', 45, 103),
+(50922, 'Mesa de comedor', 'Madera maciza', 350, 'USD', 'img/prod50922_1.jpg', 28, 103),
+(50923, 'Estantería', '5 niveles', 89, 'USD', 'img/prod50923_1.jpg', 67, 103);
+
+SET SQL_SAFE_UPDATES = 1;
